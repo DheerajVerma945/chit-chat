59cd69c12a0a5e425f6f1294229c6d517e5f627e
Dheeraj Verma <vermadheeraj945@gmail.com>
2024-12-10 14:13:44 +0530

    updated resend verification link

--- src/pages/ResetVerification.jsx
+++ src/pages/ResetVerification.jsx
@@ -51,18 +51,18 @@ const ResetVerification = () => {
       <div className="flex items-center justify-center flex-col p-6 sm:p-12">
         {showResendMail && (
           <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-            <div className="relative bg-neutral-300 p-8 rounded-lg shadow-lg w-80 max-w-sm">
+            <div className="relative bg-base-100 p-8 rounded-lg shadow-lg w-80 max-w-sm">
               <button
                 onClick={() => setShowResendMail(false)}
-                className="absolute top-2 right-2 text-base-content/60"
+                className="absolute top-2 right-2"
               >
-                <X className="size-6 text-black" />
+                <X className="size-6 text-primary" />
               </button>
               <div className="text-center">
                 <h2 className="text-xl font-semibold mb-4">
                   Resend Verification Email
                 </h2>
-                <p className="text-base-content/60 mb-4">
+                <p className="text-base-content/90 mb-4">
                   The verification link may have expired or isn't working. Click
                   below to resend the link.
                 </p>
